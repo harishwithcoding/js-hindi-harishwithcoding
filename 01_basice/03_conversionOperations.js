2449a1b44bb81878372f4665fa195cdd912b222b
harishwithcoding <hari734000@gmail.com>
2024-09-04 06:10:31 +0000

    operators

--- 01_basice/03_conversionOperations.js
+++ 01_basice/03_conversionOperations.js
@@ -13,9 +13,9 @@
 // "33abc" => NaN
 // true => 1; false => 0
 
-// let isLoggedIn = 1;
+let isLoggedIn = 1;
 
-// let boolenIsloogedIn = Boolean(isLoggedIn);
+let boolenIsloogedIn = Boolean(isLoggedIn);
 // console.log(typeof boolenIsloogedIn);
 
 // console.log(boolenIsloogedIn);
@@ -28,5 +28,24 @@ let someNumber = 33;
 
 let stringNumber = String(someNumber);
 
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+//********************************Operations*********************************
+
+let value = 33;
+let negValue = -value;
+// console.log(negValue);
+
+let str1 = "Hello!";
+let str2 = " Harish";
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");

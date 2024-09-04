272163c8882b311572a47bfb11cce42307b216dc
harishwithcoding <hari734000@gmail.com>
2024-09-04 05:31:01 +0000

    datatypes

---
+++ 01_basice/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553;
+let accountEmail = "Harish@gmail.com";
+var accountPassword = "12345";
+accountCity = "Sri Ganganagar";
+let accountState;
+
+// accountId = 2; // Not allowed
+accountEmail = "Hc@gmail.com"
+accountPassword = "232323";
+accountCity = "Jaipur";
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
---
+++ 01_basice/02_datatypes.js
@@ -0,0 +1,3 @@
+"use strict";
+
+// alert(3+3);  we are using nodejs, not browser

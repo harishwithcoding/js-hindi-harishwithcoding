d703243bd018f02875e7c4fa72304c5db143809f
harishwithcoding <hari734000@gmail.com>
2024-09-05 05:24:02 +0000

    02 Array

---
+++ 02_Array/02_Array.js
@@ -0,0 +1,27 @@
+const marvelHero = ["Iron Man", "Thor", "Hulk"];
+const DC = ["Superman", "Flash", "Batman"];
+
+// marvelHero.push(DC);
+// console.log(marvelHero);
+// console.log(marvelHero[3][1]);
+
+// const newArr = marvelHero.concat(DC);
+// console.log(newArr);
+// console.log(newArr[5]);
+
+// const allNewheros = [...marvelHero, ...DC];
+// console.log(allNewheros);
+
+const anotherArr = [1,2,3,4,[5,6],7,[6,8,4,[4,67]]];
+
+// const realArr = anotherArr.flat(Infinity);
+// console.log(realArr);
+
+// console.log(Array.isArray("Harish"));
+// console.log(Array.from("Harish")); // To make the Array use Array.from
+// console.log(Array.from({name:"Harish"}));
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+console.log(Array.of(score1, score2, score3));

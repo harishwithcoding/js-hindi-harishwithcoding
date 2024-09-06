4bef7c9d951e90b4f694f13c2bdb09b844014716
harishwithcoding <hari734000@gmail.com>
2024-09-06 10:35:30 +0000

    while loop

---
+++ 05_itertions/two.js
@@ -0,0 +1,14 @@
+// let index = 1;
+// while(index <= 10){
+//     console.log(index);
+//     index += 2;
+// }
+
+let myArr = ["Harish", "Dhoni", "Sahil"];
+
+let arr = 0;
+
+while(arr < myArr.length){
+    console.log(`value is ${myArr[arr]}`);
+    arr = arr + 1;
+}

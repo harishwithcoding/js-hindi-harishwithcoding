20c5634cb36f59faaf98072ea1b4f6c2cf63b5c7
harishwithcoding <hari734000@gmail.com>
2024-09-06 11:01:56 +0000

    forin loop

---
+++ 05_itertions/four.js
@@ -0,0 +1,17 @@
+const myObj = {
+    JS: "JavaScript",
+    cpp: "C++",
+    py: "python",
+}
+
+for (const key in myObj) {
+    // console.log(myObj[key]);
+   // console.log(`${key} for is ${myObj[key]}`);
+}
+
+const Arr = ["js", "py", "cpp", "java"];
+
+for (const key in Arr) {
+    console.log(Arr[key]);
+    
+}
